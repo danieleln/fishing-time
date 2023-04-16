838c171d39d0ad3b39755610b4cb6a75aa01e9c5
danieleln <daniele.monzani@outlook.com>
2023-04-16 11:43:40 +0200

    Fixed BUG in _setup_fish.bash

--- src/_setup_fish.bash
+++ src/_setup_fish.bash
@@ -33,14 +33,14 @@ fi
 
 
 # changing default shell to fish
-if [ "$SHELL" != $(which fish) ]; then
+if [ "$SHELL" != "$(which fish)" ]; then
     echo "Enter your password to change the default shell to fish:"
     echo -e "\n\n\nCHANGING DEFAULT SHELL TO FISH"      >>  $LOG_FILE
     chsh -s $(which fish)                               2>> $LOG_FILE
 
 
     # verifying if the shell was changed succesfully
-    if [ "$SHELL" != $(which fish) ]; then
+    if [ "$SHELL" != "$(which fish)" ]; then
         echo "SHELL WAS NOT CHANGED."                       >>  $LOG_FILE
         echo "CURRENT SHELL IS $SHELL"                      >>  $LOG_FILE
         echo "Some errors occurred while changing the default shell to fish."
