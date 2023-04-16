59850bdfe5250cfdb603ae49f2c3d4d4149c5bcf
danieleln <daniele.monzani@outlook.com>
2023-04-16 11:37:16 +0200

    Improved readability of fishing_time.bash

--- fishing_time.bash
+++ fishing_time.bash
@@ -1,16 +1,19 @@
 #!/usr/bin/bash
 
 
-
+# log file
 LOG_FILE="/tmp/fishing_time.log"
 
 # path of the current script
 __HERE__=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+
+
 # initializing the log file
-echo "fishing-time"                                         >  $LOG_FILE
-date                                                        >> $LOG_FILE
+echo "fishing-time" >  $LOG_FILE
+date                >> $LOG_FILE
 
 
+# install fish and make it the default shell
 chmod u+x ${__HERE__}/src/_setup_fish.bash
 ${__HERE__}/src/_setup_fish.bash ${LOG_FILE}
