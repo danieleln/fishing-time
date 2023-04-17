f7263d9315487240f7dc56c55ec9a31b1fa5209d
danieleln <daniele.monzani@outlook.com>
2023-04-17 13:44:45 +0200

    mod: _bulk_install.bash now checks if programs are already installed

--- src/_bulk_install.bash
+++ src/_bulk_install.bash
@@ -14,26 +14,30 @@ declare -r __HERE__=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null 
 echo -e "\n\n\nINSTALLING PROGRAMS" >> $LOG_FILE
 
 
-# iterate all the file in the src folder
+# iterating all the file in the src folder
 for file in ${__HERE__}/*; do
     filename="$(basename $file)"
 
-    # select files whose name starts with "_install_" and
+    # selecting files whose name starts with "_install_" and
     # ends with ".bash"
     if [[ "$filename" == _install_*.bash ]]; then
 
-        # extract the program name
+        # extracting the program name
         program_name="${filename:9:-5}"
 
-        # asking the user confirmation to install the program
-        echo -e "\n\nDo you want to install $program_name? [y/n]"
-        read answer
-    
-        if [[ "$answer" == 'y' || "$answer" == 'Y' ]]
-        then
-            # run the installation file
-            chmod u+x $file
-            $file
+        # verifying if the program is already installed
+        if ! which "$program_name" > /dev/null; then
+            # asking the user confirmation to install the program
+            echo -e "\n\nDo you want to install $program_name? [y/n]"
+            read answer
+        
+            if [[ "$answer" == 'y' || "$answer" == 'Y' ]]
+            then
+                echo -e "\nINSTALLING $program_name" >> $LOG_FILE
+                # running the installation file
+                chmod u+x $file
+                $file
+            fi
         fi
 
     fi
