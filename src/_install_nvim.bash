3fb19a61f49558b07d71db4ef9af9a9aace8de70
danieleln <daniele.monzani@outlook.com>
2023-04-27 13:21:29 +0200

    mod: nvim requires libfuse2 to run from the appimage

--- src/_install_nvim.bash
+++ src/_install_nvim.bash
@@ -6,6 +6,9 @@ LOG_FILE="$1"
 
 cd /tmp
 
+# installing nvim from the appimage also require libfuse2 to run
+sudo apt install libfuse2
+
 # downloading the latest stable release of neovim
 wget --quiet https://github.com/neovim/neovim/releases/download/stable/nvim.appimage --output-document nvim
 
