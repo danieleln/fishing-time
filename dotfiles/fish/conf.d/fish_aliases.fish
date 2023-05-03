cffb39de1870902f2df00bacc0aa7697d3339fcd
danieleln <daniele.monzani@outlook.com>
2023-05-03 15:11:11 +0200

    mod: added abbr for cp, mkdir

--- dotfiles/fish/conf.d/fish_aliases.fish
+++ dotfiles/fish/conf.d/fish_aliases.fish
@@ -1,27 +1,27 @@
 #!/usr/bin/env fish
 
-# {{ LISTING ABBR }}
-
-# list (equivalent to ls, but uses exa)
+# {{ list abbr }}
 function l
+    # list (equivalent to ls, but uses exa)
     exa -1F --group-directories-first -s extension --icons $argv
 end
 funcsave -q l
 
-abbr ll   l -lF   # list long
-abbr la   l -a    # list all
-abbr lla  l -la   # list long all
-abbr lt   l -T    # list tree (equivalent to tree)
-abbr llt  l -lT   # list long tree
-abbr lta  l -Ta   # list tree all
-abbr llta l -lTa  # list long tree all
+abbr ls   l         # list (equivalent to l)
+abbr la   l -a      # list all
+abbr ll   l -lF     # list long
+abbr lla  l -la     # list long all
+abbr lt   l -T      # list tree (equivalent to tree)
+abbr llt  l -lT     # list long tree
+abbr lta  l -Ta     # list tree all
+abbr llta l -lTa    # list long tree all
 
 
 
 
-# {{ CLEAR SCREEN (AND LIST) }}
+# {{ clear screen (and list) }}
 
-abbr c "clear" # clear the screen
+abbr c "clear"      # clear the screen
 
 # clear and list
 function cl
@@ -31,23 +31,30 @@ function cl
 end
 funcsave -q cl
 
+abbr cls   cl       # clear and list (equivalent to cl)
 abbr cll   cl -lF   # clear and list long
 abbr cla   cl -a    # clear and list all
 abbr clla  cl -la   # clear and list long all
-abbr clt   cl -T    # clear and list tree (equivalent to tree)
+abbr clt   cl -T    # clear and list tree
 abbr cllt  cl -lT   # clear and list long tree
 abbr clta  cl -Ta   # clear and list tree all
 abbr cllta cl -lTa  # clear and list long tree all
 
 
 
-# {{ NEOVIM ABBR }}
+# {{ cp, mkdir abbr }}
+abbr cp "cp -r"       # copy is now recursive by default
+abbr mkdir "mkdir -p" # mkdir now builds also the parent dir by default
+
+
+
+# {{ neovim abbr }}
 abbr vim "nvim"
 abbr vi  "nvim"
 
 
 
-# {{ PARENT DIRECTORY ABBR }}
+# {{ parent directory abbr }}
 abbr ..  "cd .."
 abbr ... "cd ../.."
 abbr .1  "cd .."
@@ -60,6 +67,6 @@ abbr .7  "cd ../../../../../../.."
 
 
 
-# {{ EXIT ABBR }}
+# {{ exit abbr }}
 abbr exit "exit 0"
 abbr quit "exit 0"
